9fe2df564bf8573cf49f05589801a96b981af370
Elodie Favier <elodie.favier@mailbox.org>
2024-10-08 10:45:32 +0200

    creatio page about et liens

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+'use client'
+
+export default function About (){
+     return (
+        <div className="">
+
+<h2>À propos de moi</h2> 
+
+<p>Je m'appelle Marienka, et la photographie est pour moi une manière de capturer des instants précieux du quotidien. Toujours équipée de mon appareil photo, je pars à la découverte des scènes qui, à mes yeux, sont chargées d'émotions. J'aime immortaliser ces moments furtifs, ces regards ou ces lumières qui racontent une histoire, sans artifices. Pour moi, la photographie est un moyen d’explorer et de partager la beauté simple du monde qui nous entoure.</p>
+
+<p>
+Ce site est simplement un espace pour partager mon travail et, je l'espère, pour vous transmettre un peu des émotions que je ressens en prenant ces photos.</p>
+
+<blockquote>
+        <p>
+              "Le merveilleux est dans l'ordinaire."
+– Robert Doisneau  
+        </p>
+</blockquote>
+
+        </div>
+     )
+}
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,11 +1,26 @@
+'use client'
+import Link from 'next/link';
+import { useRouter } from "next/navigation";
+import Image from 'next/image';
+
 export default function NavBar() {
+
+  const router = useRouter()
     return (
    <ul className="flex flex-row m-4 text-xl items-center">
-    <li className="ml-4 ">Acceuil</li>
-    <li className="ml-4">A propos</li>
-    <li className="ml-4">Instagram</li>
+    <Link href="/" className="ml-4 ">Accueil</Link>
+    <Link href="/about" className="ml-4">A propos</Link>
+    <Link href="https://mastodon.social/@Annouck" className="ml-4"><Image 
+        src="/img/logo-mastodon.svg" 
+        alt="logo du reseau social mastodon"
+        width={32}
+        height={32}
+        style={{objectFit:"cover"}}
+          sizes="100vw"
+          />
+</Link>
    </ul>
-      
-    );
+    )
+    
   }
   
